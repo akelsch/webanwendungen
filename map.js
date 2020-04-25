3d640c9aae52153a0c6a4a2509ede5e6c6205def
Arthur Kelsch <akelsch@users.noreply.github.com>
2020-04-25 14:17:16 +0200

    Cleanup

--- map.js
+++ map.js
@@ -1,53 +1,47 @@
 import { mapData } from './map-data.js'
 import { createSVGElement, normalize } from './common.js'
-const countyByRings = new Map()
+
 const svg = document.querySelector('#map')
-console.log(svg)
 
+const countyGeometryMapping = new Map()
 
-const fillMap = () => {
+function initalizeMapping () {
   mapData.features.forEach(element => {
-    countyByRings.set(element.attributes.county, element.geometry.rings)
-  });
+    countyGeometryMapping.set(element.attributes.county, element.geometry.rings)
+  })
 }
 
-fillMap()
-
-function minMax(value, XY) {
-  let min, max = 0;
-  //The values for Germany
-  if (XY === "X") {
-    min = 5.8;
-    max = 15.1;
-  }
-  else if (XY === "Y") {
-    min = 47.2;
-    max = 55.1;
-  }
-  return ((value - min) / (max - min));
-}
-
-
-const createCountyOutline = () => {
-  let attr = 'M '
-
-  for (let [key, value] of countyByRings) {
-    value.forEach(v => {
-      v.forEach(point => {
-        attr += `${minMax(point[0],'X') * 100},${minMax(point[1],'Y') * 100} L`
+function renderCounties () {
+  for (const [_, rings] of countyGeometryMapping) {
+    rings.forEach(ring => {
+      let pathData = 'M '
+      ring.forEach(([x, y]) => {
+        pathData += `${minMax(x, 'X') * 100},${minMax(y, 'Y') * 100} `
       })
-      attr += 'Z'
-      const path = createSVGElement('path', { d: attr, stroke: 'pink', fill: 'red' })
+      pathData += 'Z'
+
+      const path = createSVGElement('path', {
+        fill: 'red',
+        stroke: 'pink',
+        d: pathData
+      })
       svg.appendChild(path)
-      attr = 'M '
     })
   }
-  //let values = countyByRings.get('SK Flensburg')
-  //values.forEach(point => attr+= `${point[0]},${point[1]} L `)
-  //attr+='Z'
-  //const path = createSVGElement('path',{d:attr ,stroke : 'pink' })
-  //svg.appendChild(path)
 }
 
-createCountyOutline()
+function minMax (value, XY) {
+  let min; let max = 0
+  // The values for Germany
+  if (XY === 'X') {
+    min = 5.8
+    max = 15.1
+  } else if (XY === 'Y') {
+    min = 47.2
+    max = 55.1
+  }
+  return ((value - min) / (max - min))
+}
 
+initalizeMapping()
+renderCounties()
