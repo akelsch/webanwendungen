930662e10260baad05e1ce9cd0278c0be4fe0714
mahanKarimi <mahandk128@gmail.com>
2020-04-25 12:51:29 +0200

    create for a county a path element

--- map.js
+++ map.js
@@ -1,1 +1,23 @@
 import { mapData } from './map-data.js'
+import {createSVGElement} from './common.js'
+const countyByRings = new Map()
+const svg = document.querySelector('#bar-chart')
+
+
+const fillMap = () => {
+  mapData.features.forEach(element => {
+    countyByRings.set(element.attributes.county, element.geometry.rings)
+  });
+}
+
+fillMap()
+
+const createCountyOutline = () => {
+  let attr = 'M '
+  let values = countyByRings.get('SK Flensburg')
+  values[0].forEach(([x,y]) => attr+= `${x},${y} L `)
+  attr+='Z'
+  const path = createSVGElement('path',{d:attr ,stroke : 'pink' })
+  svg.appendChild(path)
+}
+createCountyOutline()
