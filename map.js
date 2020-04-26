3a7e3713bde736f7db9e13085b66b9b928c839ca
mahanKarimi <mahandk128@gmail.com>
2020-04-27 00:41:19 +0200

    add new Model , and content for popup window

--- map.js
+++ map.js
@@ -1,13 +1,20 @@
 import { mapData } from './map-data.js'
 import { createSVGElement, normalize, determineMinMaxCoordinates } from './common.js'
-
+import PopUpData from './model/popUpData.js'
 // DOM
 const svg = document.querySelector('#map')
 
 // Data
 const countyGeometryMapping = new Map()
 mapData.features.forEach(element => {
-  countyGeometryMapping.set(element.attributes.county, element.geometry.rings)
+  const countyData = element.attributes
+  countyGeometryMapping.set(new PopUpData(countyData.county,
+    countyData.BL,
+    countyData.EWZ,
+    countyData.cases,
+    countyData.cases_per_100k,
+    countyData.cases7_per_100k,
+    countyData.deaths), element.geometry.rings)
 })
 
 // Start
@@ -15,7 +22,7 @@ drawMap() // TODO options
 
 function drawMap () {
   const minMaxData = determineMinMaxCoordinates(countyGeometryMapping)
-  for (const rings of countyGeometryMapping.values()) {
+  for (const [popUpData,rings] of countyGeometryMapping) {
     rings.forEach(ring => {
       let pathData = 'M '
       ring.forEach(([x, y]) => {
@@ -29,7 +36,27 @@ function drawMap () {
         'stroke-width': 0.1,
         d: pathData
       })
+      // create tooltip element
+      const ttBox = document.createElement("div");
+      // set style
+      
+      path.addEventListener('mouseover', () => console.log(popUpData.county))
+      path.addEventListener('click', () =>{showPopUpWindow(path,popUpData)})
       svg.appendChild(path)
     })
   }
+}
+
+function showPopUpWindow(path,popUpData){
+  const divPopUpWindow = document.createElement('div')
+  const closePopUpWindowButton = document.createElement('button')
+  divPopUpWindow.innerText = `Bezirk = ${popUpData.county}
+                              Bundesland = ${popUpData.bl}
+                              Einwohnerzahl = ${popUpData.numberOfResidents}
+                              Anzahl aller Infizierten = ${popUpData.allInfects} 
+                              Anzahl der Infizierten pro 100.000 Einwohner = ${popUpData.infectsBy100kInhabitant}
+                              Anzahl der Infizierten pro 100.000 Einwohner der letzten 7 Tage = ${popUpData.infectsBy100kInhabitantLast7Days}
+                              Anzahl der Toten = ${popUpData.deads}`
+  divPopUpWindow.appendChild(closePopUpWindowButton)
+                              
 }
---
+++ model/popUpData.js
@@ -0,0 +1,14 @@
+class PopUpData {
+
+  constructor(county,bl,numberOfResidents,allInfects,infectsBy100kInhabitant,infectsBy100kInhabitantLast7Days,deads){
+    this.county = county
+    this.bl = bl 
+    this.numberOfResidents = numberOfResidents
+    this.allInfects = allInfects
+    this.infectsBy100kInhabitant = infectsBy100kInhabitant
+    this.infectsBy100kInhabitantLast7Days = infectsBy100kInhabitantLast7Days
+    this.deads = deads 
+  }
+}
+
+export default PopUpData 
