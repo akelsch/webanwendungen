270a77307b641d9a6ba2234ed578fa6ec2c561d8
Arthur Kelsch <akelsch@users.noreply.github.com>
2020-04-26 16:03:44 +0200

    Finish bar chart transition

--- bc.js
+++ bc.js
@@ -2,15 +2,16 @@
 import { mapData } from './map-data.js'
 import { createSVGElement, normalize } from './common.js'
 
+// DOM
 const select = document.getElementById('state')
 const svg = document.getElementById('bar-chart')
 
-const barChartTimeoutInMs = 200
+// CSS
+const barChartTransitionInMs = 750
 
+// Data
 const sortedData = mapData.features.map(elem => elem.attributes)
   .sort((x, y) => y.cases_per_100k - x.cases_per_100k)
-
-const maxCases = sortedData[0].cases_per_100k
 
 function initSelect () {
   const states = new Set()
@@ -23,19 +24,32 @@ function drawBarChart ({ state = 'alle', amount = 5 }) {
   let casesY = 11
   let countyY = 7
   let barY = 8
+  let position = 0
 
   const topN = sortedData.filter(county => county.BL === state || state === 'alle').slice(0, amount)
   topN.forEach(county => {
-    const group = createSVGElement('g')
+    const group = createSVGElement('g', {
+      opacity: 1,
+      style: 'opacity: 0;'
+    })
+
     group.appendChild(createCasesText(casesY, county))
     casesY += 7
+
     group.appendChild(createCountyText(countyY, county))
     countyY += 7
+
     const bar = createBarRect(barY, county)
     group.appendChild(bar)
     barY += 7
+
     svg.appendChild(group)
-    setTimeout(() => bar.removeAttribute('style'), barChartTimeoutInMs)
+
+    // Referenz: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Transitions/Using_CSS_transitions#JavaScript_examples
+    window.setTimeout(() => {
+      group.removeAttribute('style')
+      bar.removeAttribute('style')
+    }, barChartTransitionInMs * position++)
   })
 }
 
@@ -63,13 +77,13 @@ function createCountyText (y, county) {
 
 function createBarRect (y, county) {
   const rect = createSVGElement('rect', {
-    width: normalize(county.cases_per_100k, 0, maxCases) * 69,
+    width: normalize(county.cases_per_100k, 0, sortedData[0].cases_per_100k) * 69,
     height: 3,
     x: 10,
     y: y,
     fill: 'lightblue',
     class: 'bar',
-    style: 'width: 0'
+    style: 'width: 0;'
   })
   return rect
 }
