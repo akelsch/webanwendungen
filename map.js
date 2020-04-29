b6dd1df95a543c3615f758f3326154bb12f857f7
Arthur Kelsch <akelsch@users.noreply.github.com>
2020-04-29 19:58:28 +0200

    Popup with table

--- map.js
+++ map.js
@@ -1,22 +1,22 @@
 import { mapData } from './map-data.js'
-import { createSVGElement, normalize, determineMinMaxCoordinates } from './common.js'
+import { createSVGElement, normalize, createTable, appendRowToTable } from './common.js'
 import PopupData from './model/PopupData.js'
 
 // DOM
 const svg = document.querySelector('#map')
 
 // Data
-const countyGeometryMapping = new Map()
+const data = new Map()
 mapData.features.forEach(element => {
-  countyGeometryMapping.set(new PopupData(element.attributes), element.geometry.rings)
+  data.set(new PopupData(element.attributes), element.geometry.rings)
 })
 
 // Start
 drawMap() // TODO options
 
 function drawMap () {
-  const minMaxData = determineMinMaxCoordinates(countyGeometryMapping)
-  for (const [popupData, rings] of countyGeometryMapping) {
+  const minMaxData = determineMinMaxCoordinates(data.values())
+  for (const [popupData, rings] of data) {
     rings.forEach(ring => {
       let pathData = 'M '
       ring.forEach(([x, y]) => {
@@ -37,6 +37,33 @@ function drawMap () {
   }
 }
 
+function determineMinMaxCoordinates (data) {
+  let minX = Infinity
+  let minY = Infinity
+  let maxX = 0
+  let maxY = 0
+  for (const rings of data) {
+    rings.forEach(ring => {
+      ring.forEach(([x, y]) => {
+        if (minX > x) {
+          minX = x
+        } else if (maxX < x) {
+          maxX = x
+        }
+
+        if (minY > y) {
+          minY = y
+        } else if (maxY < y) {
+          maxY = y
+        }
+      })
+    })
+  }
+  return {
+    minX, maxX, minY, maxY
+  }
+}
+
 function displayPopup (popupData) {
   const popupDom = document.querySelector('#popup')
   if (popupDom) {
@@ -45,18 +72,22 @@ function displayPopup (popupData) {
 
   const popupDiv = document.createElement('div')
   popupDiv.id = 'popup'
-  popupDiv.innerText = `Bezirk = ${popupData.county}
-                        Bundesland = ${popupData.bl}
-                        Einwohnerzahl = ${popupData.ewz}
-                        Anzahl aller Infizierten = ${popupData.cases}
-                        Anzahl der Infizierten pro 100.000 Einwohner = ${popupData.cases_per_100k.toFixed(2)}
-                        Anzahl der Infizierten pro 100.000 Einwohner der letzten 7 Tage = ${popupData.cases7_per_100k.toFixed(2)}
-                        Anzahl der Toten = ${popupData.deaths}`
 
   const closePopupButton = document.createElement('button')
   closePopupButton.textContent = 'x'
   closePopupButton.onclick = () => popupDiv.remove()
+
+  const dataTable = createTable()
+  appendRowToTable(dataTable, 'Kreis', popupData.county)
+  appendRowToTable(dataTable, 'Bundesland', popupData.bl)
+  appendRowToTable(dataTable, 'Einwohnerzahl', popupData.ewz)
+  appendRowToTable(dataTable, 'Fälle', popupData.cases)
+  appendRowToTable(dataTable, 'Fälle/100T Einwohner', popupData.cases_per_100k.toFixed(1))
+  appendRowToTable(dataTable, 'In den letzten 7 Tagen', popupData.cases7_per_100k.toFixed(1))
+  appendRowToTable(dataTable, 'Todesfälle', popupData.deaths)
+
   popupDiv.appendChild(closePopupButton)
+  popupDiv.appendChild(dataTable)
 
   svg.parentNode.insertBefore(popupDiv, svg)
 }
